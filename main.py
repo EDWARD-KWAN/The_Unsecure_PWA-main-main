a4561f273de9b7a03f8d78afcac5c5df35d68b3f
Edward Kwan <edward.kwan@education.nsw.gov.au>
2025-04-03 23:07:13 +1100

    PWA implement strong password

---
+++ main.py
@@ -0,0 +1,72 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+import re
+import user_management as dbHandler
+
+
+password_regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+app = Flask(__name__)
+
+
+@app.route("/success.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
+def addFeedback():
+    if request.method == "GET" and request.args.get("url"):
+        url = request.args.get("url", "")
+        return redirect(url, code=302)
+    if request.method == "POST":
+        feedback = request.form["feedback"]
+        dbHandler.insertFeedback(feedback)
+        dbHandler.listFeedback()
+        return render_template("/success.html", state=True, value="Back")
+    else:
+        dbHandler.listFeedback()
+        return render_template("/success.html", state=True, value="Back")
+
+
+@app.route("/signup.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
+def signup():
+    if request.method == "GET" and request.args.get("url"):
+        url = request.args.get("url", "")
+        return redirect(url, code=302)
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        DoB = request.form["dob"]
+
+                
+        if not password_regex.match(password): #express not correct password
+            
+            return render_template("/signup.html") # If password is invalid, sent back to try again
+
+        dbHandler.insertUser(username, password, DoB)
+        dbHandler.insertUser(username, password, DoB)
+        return render_template("/index.html")
+    else:
+        return render_template("/signup.html")
+
+
+@app.route("/index.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "GET" and request.args.get("url"):
+        url = request.args.get("url", "")
+        return redirect(url, code=302)
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        isLoggedIn = dbHandler.retrieveUsers(username, password)
+        if isLoggedIn:
+            dbHandler.listFeedback()
+            return render_template("/success.html", value=username, state=isLoggedIn)
+        else:
+            return render_template("/index.html")
+    else:
+        return render_template("/index.html")
+
+
+if __name__ == "__main__":
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+    app.run(debug=True, host="0.0.0.0", port=5000)
